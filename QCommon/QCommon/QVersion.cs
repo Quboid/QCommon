6dc271c456ca79d2bdc5c2b97af0937b80b880d9
Quboid <github@quboid.net>
2022-10-15 23:36:36 +0100

    Fixed QVersion assembly reference

--- QCommon/QCommon/QVersion.cs
+++ QCommon/QCommon/QVersion.cs
@@ -14,7 +14,11 @@ namespace QCommonLib
 
         public static string Version()
         {
-            Assembly ass = Assembly.GetExecutingAssembly();
+            return Version(Assembly.GetCallingAssembly());
+        }
+
+        public static string Version(Assembly ass)
+        {
             if (ass.GetName().Version.Minor == 0 && ass.GetName().Version.Build == 0)
             {
                 return ass.GetName().Version.Major.ToString() + ".0";
